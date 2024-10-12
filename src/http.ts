894a7ce900e20b254016349bdbaa4a0d004f4d7e
Shivansh Singh <singhshivansh12may@gmail.com>
2024-10-12 19:38:48 +0530

    Updated ran route

--- src/http.ts
+++ src/http.ts
@@ -1,24 +1,39 @@
-import { Express,Request,Response } from "express";
+import { Express, Request, Response } from "express";
 import express from "express";
 import data from "./NithAlumni";
 
 export function initHttp(app: Express) {
     app.use(express.json());
-    app.get("/test", (req, res) => {
+
+    app.get("/test", (req: Request, res: Response) => {
         res.json(data);
     });
+
     app.post("/ran", (req: Request, res: Response) => {
         const excludeIndexes: number[] = req.body.indexes || [];
+        const maxToReturn: number = 10;
         
-        // Filter out the excluded indexes
-        const availableData = data.filter((_, index) => !excludeIndexes.includes(index));
+        // Create an array of available indexes
+        const availableIndexes = data.reduce((acc, _, index) => {
+            if (!excludeIndexes.includes(index)) {
+                acc.push(index);
+            }
+            return acc;
+        }, [] as number[]);
 
-        // Shuffle the available data
-        const shuffled = [...availableData].sort(() => 0.5 - Math.random());
+        // Shuffle the available indexes
+        const shuffledIndexes = [...availableIndexes].sort(() => 0.5 - Math.random());
 
-        // Get the first 10 items (or less if there are fewer than 10 available)
-        const randomItems = shuffled.slice(0, 10);
+        // Get up to 10 indexes, or all remaining if less than 10
+        const selectedIndexes = shuffledIndexes.slice(0, Math.min(maxToReturn, shuffledIndexes.length));
 
-        res.json(randomItems);
+        // Get the items corresponding to the selected indexes
+        const selectedItems = selectedIndexes.map(index => data[index]);
+
+        res.json({
+            items: selectedItems,
+            indexes: selectedIndexes,
+            remaining: availableIndexes.length - selectedIndexes.length
+        });
     });
 }
