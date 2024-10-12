a521316db195277f2b950fd4adaee4456e00b0d1
Shivansh Singh <singhshivansh12may@gmail.com>
2024-10-12 19:11:12 +0530

    Added random route

--- src/http.ts
+++ src/http.ts
@@ -1,4 +1,4 @@
-import { Express } from "express";
+import { Express,Request,Response } from "express";
 import express from "express";
 import data from "./NithAlumni";
 
@@ -7,5 +7,18 @@ export function initHttp(app: Express) {
     app.get("/test", (req, res) => {
         res.json(data);
     });
-   
+    app.post("/ran", (req: Request, res: Response) => {
+        const excludeIndexes: number[] = req.body.indexes || [];
+        
+        // Filter out the excluded indexes
+        const availableData = data.filter((_, index) => !excludeIndexes.includes(index));
+
+        // Shuffle the available data
+        const shuffled = [...availableData].sort(() => 0.5 - Math.random());
+
+        // Get the first 10 items (or less if there are fewer than 10 available)
+        const randomItems = shuffled.slice(0, 10);
+
+        res.json(randomItems);
+    });
 }
